7180bf0e376539635e93ff6bc94ed5451d197d95
davidg10109 <47329229+davidg10109@users.noreply.github.com>
2019-02-21 11:56:53 -0500

    Update emaillist.php

--- emaillist.php
+++ emaillist.php
@@ -2,183 +2,279 @@
 <html>
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/connectdb.php";
-$imgdir = "/files/images/";
 ?>
-<?php 
-	if (strlen($_POST['FileSel'])>0 || strlen($_POST['fileselcopy'])>0) {		
-		//# gets POST and gets contents as $page and email array
-		$emailarr = array();
-		$emailarr = $_POST['emaillist'];
-		$filesel = $_SERVER['DOCUMENT_ROOT'] . "/files/" . $_POST['FileSel'];
-		$emailsubj;
-		$emailrep;
-		$emailcc;
-		//# if the fileselcopy is sent, change $emailarr and $filesel
-		if(isset($_POST['fileselcopy'])) {
-		    $emailarr = $_POST['emaillistcopy'];
-		    $filesel = $_SERVER['DOCUMENT_ROOT'] . "/files/" . $_POST['fileselcopy'];
-		}
-		//# grab correct email detail info
-		if(strlen($_POST['emailsubj1'])>0) {
-		    $emailsubj = $_POST['emailsubj1'];
-		    $emailrep = $_POST['emailrep1'];
-		    $emailcc = $_POST['emailcc1'];
-		}
-		else if (strlen($_POST['emailsubj2'])>0) {
-		    $emailsubj = $_POST['emailsubj2'];
-		    $emailrep = $_POST['emailrep2'];
-		    $emailcc = $_POST['emailcc2'];
-		}
-		//# get send date and file -> $page
-		$senddate = $_POST['senddate'];
-		$page = file_get_contents($filesel);
-		
-		//# fix image directory
-		$page2 = str_replace("src=\"images/image1.png\"","src=\"".$imgdir."image1.png"."\"",$page);
-		
-		//# put emailllist into an array split on ,
-		$emailsplit = explode(",",$emailarr);
-		
-		//# display an Email Preview with inserted variables
-		$pageshow = insertVars($conn,$page2,$emailsplit[0]);
-		echo $pageshow;
-		
-		//# if the Send button is submitted, put the entries into EmailList for scheduled delivery
-		if(isset($_POST['schedule'])) {
-		    foreach($emailsplit as $e) {
-		        //$e = varFilter($e);
-		        $page = insertVars($conn,$page2,$e);
-		        setCron($conn,$e,$page,$senddate,1,$emailsubj,$emailrep,$emailcc);
-		    }
-		}
-		//# if the Send Now button is pressed, send an email now (urgency=0)
-		if(isset($_POST['sendnow'])) {
-		    foreach($emailsplit as $e) {
-		        $page = insertVars($conn,$page2,$e);
-		        setCron($conn,$e,$page,0,0,$emailsubj,$emailrep,$emailcc);
-		    }
-		}
+<head>
+<style type="text/css">
+* {
+	margin: 0; padding: 0;
+}
+
+#container {
+	height: 100%; width:100%; font-size: 0;
+}
+
+#left, #middle, #right {
+	display: inline-block; *display: inline; zoom: 1; vertical-align: top; font-size:16px;;
+}
+
+#left {
+	width: 25%;
+	position: relative;
+	left: 5px;
+}
+
+#middle {
+	width: 15%;
+}
+
+#right {
+	position: relative;
+	width: 60%;
+	right: 5px;
+}
+
+.tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+    font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+    background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+    background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+
+iframe {
+	height: 100px;
+}
+
+form.fclass {
+    border-style: groove;
+    border: 1px solid #000;
+}
+
+.centersubmit {
+    margin: 0px auto;
+    text-align:center;
+}
+
+fieldset {
+    display:table;
+    border: 0px;
+}
+
+div.column {
+    display:table-cell;
+}
+
+div.column > input {
+    width:145%;
+    padding: 2px;
+}
+
+div.column2 {
+    display:table-cell;
+    text-align:right;
+}
+
+div.row {
+    display:table-row;
+}
+
+textarea {
+    padding: 2px;
+}
+</style>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="<?php $_SERVER["DOCUMENT_ROOT"]?>/sc/datepicker_script.js"></script>
+<script>
+  $( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
 	}
-	
-	//# filter the array of values with mysql escape strings, html filter, and trim excess whitespace
-    function varFilter($s){
-        $val = $s;
-    	$val = mysqli_real_escape_string($conn,$val);
-    	$val = filter_var($val,FILTER_SANITIZE_STRING);
-    	return trim($val);
+);
+</script>
+<script>
+function openTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-	
-	function insertVars($conn,$page,$email) {
-	    $em = $email;
-		# get all information required from Student,Passports,Program
-		if (!mysqli_query($conn,"Select * from Student where Email='$em'")) {
-			echo("Error retrieving Student data: " . mysqli_error($con));
-		}
-		else {
-			$studentquery = mysqli_query($conn,"Select * from Student where Email='$em'");
-			$studentrow = mysqli_fetch_array($studentquery);
-			$eagleid = $studentrow['EagleID'];
-			
-			$applicquery = mysqli_query($conn,"Select * from Application where EagleID='$eagleid'");
-    		$applicrow = mysqli_fetch_array($applicquery);
-    		$progid = $applicrow['ProgramID'];
-    		
-			if (!mysqli_query($conn,"Select * from Passports where EagleID='$eagleid'")) {
-				echo("Error retrieving Passport data: " . mysqli_error($con));
-			}
-			else {
-    			$passquery = mysqli_query($conn,"Select * from Passports where EagleID='$eagleid'");
-    			$passportrow = mysqli_fetch_array($passquery);
-    			if (!mysqli_query($conn,"Select * from Program where ProgramID='$progid'")) {
-    				echo("Error retrieving Program data: " . mysqli_error($con));
-    			}
-    			else {
-    				$programquery = mysqli_query($conn,"Select * from Program where ProgramID=$progid");
-    				$programrow = mysqli_fetch_array($programquery);
-    				//# set values to look for and appropriate values
-    				$valmap = array("*|Eagle ID|*",$studentrow['EagleID'],
-    				"*|First Name|*",$studentrow['FirstName'],
-    				"*|Middle Name|*",$studentrow['MiddleName'],
-    				"*|Last Name|*",$studentrow['LastName'],
-    				"*|Preferred Name|*",$studentrow['PrefName'],
-    				"*|Email|*",$studentrow['Email'],
-    				"*|Phone Number|*",$studentrow['PhoneNum'],
-    				"*|First Name as shown in passport|*",$passportrow['PassFName'],
-    				"*|Middle Name as shown in passport|*",$passportrow['PassMName'],
-    				"*|Last Name as shown in passport|*",$passportrow['PassLName'],
-    				"*|Passport Nationality|*",$passportrow['PassportNatl'],
-    				"*|Passport Number|*",$passportrow['PassportNum'],
-    				"*|Passport Expiration Date|*",$passportrow['PassportExpDate'],
-    				"*|Program Name|*",$programrow['ProgramName'],
-    				"*|Semester|*",$programrow['Semester'],
-    				"*|1st Payment Amount|*",$programrow['Payment1Amt'],
-    				"*|1st Payment Date|*",$programrow['Payment1DateDue'],
-    				"*|2nd Payment Amount|*",$programrow['Payment2Amt'],
-    				"*|2nd Payment Date|*",$programrow['Payment2DateDue'],
-    				"*|3rd Payment Amount|*",$programrow['Payment3Amt'],
-    				"*|3rd Payment Date|*",$programrow['Payment3DateDue'],
-    				"*|4th Payment Amount|*",$programrow['Payment4Amt'],
-    				"*|4th Payment Date|*",$programrow['Payment4DateDue'],
-    				"*|Insurance Amount|*",$programrow['InsuranceAmt'],
-    				"*|Insurance Payment Date|*",$programrow['InsuranceDateDue']);
-    				//# call count once to reduce load
-    				$ct = count($valmap);
-    				//# iterate through valmap to make date values easier to read
-    				for($i=0;$i<$ct;$i++) {
-    					if ((stripos($valmap[$i],"date")) !== false) {
-    						$dttemp = date_create_from_format('Y-m-j',$valmap[$i+1]);
-    						$valmap[$i+1] = date_format($dttemp,'F jS\, Y');
-    					}
-    				}
-				//# iterate through valmap to look for values to replace in $page
-				for($i=0;$i<$ct;$i+=2) {
-					 if ((stripos($page,$valmap[$i])) !== false) {
-						$pagetemp = str_ireplace($valmap[$i],$valmap[$i+1],$page);
-						$page = $pagetemp;
-					}
-				}
-				return $page;
-				}
-			}
-		}
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
+<script type="text/javascript">
+function filedisplay() {
+    var m = document.getElementById("FileSel").value
+	if (m == "default000"){
+		document.getElementById('ifrbase').src = "";
+		document.getElementById('ifrbase').style.visibility = "hidden";
+		document.getElementById('hiddenfile').value = m;
 	}
-	
-	function setCron($conn,$email,$message,$senddate,$urgency,$subj,$rep,$cc) {
-		$subject = $subj;
-		$from = 'no-reply-oips@georgiasouthern.edu';
-		//# To send HTML mail, the Content-type header must be set
-		$headers = "MIME-Version: 1.0 \r\n";
-        $headers .= "Content-type: text/html; charset=iso-8859-1 \r\n";
-		//# Create email headers
-		$headers .= "From: " . $from . "\r\n" .
-			"Reply-to:" . $rep . "\r\n";
-			//'CC:' . $cc . "\r\n";
-		
-		//# an urgency flag=1 means it is scheduled
-		if($urgency == 1) {
-    		$eid;
-    		//# Get EagleID from Email
-    		if (!mysqli_query($conn,"Select EagleID from Student where Email='$email'")) {
-    				echo("Error retrieving EagleID from sent email: $email");
-    			}
-    		else {
-    			$q = mysqli_query($conn,"Select EagleID from Student where Email='$email'");
-    			$r = mysqli_fetch_array($q);
-    			$eid = $r['EagleID'];
-    		}
-    		//# insert entry into EmailList table
-    		if (!mysqli_query($conn,"INSERT into EmailList (EagleID,EmailTo,EmailSubject,EmailMessage,EmailHeaders,SendDate) VALUES ('$eid','$email','$subject','$message','$headers','$senddate')")) {
-    				echo("Error putting in automatic email entry: " . mysqli_error($conn));
-    			}
-    		else {
-    			mysqli_query($conn,"INSERT into EmailList (EagleID,EmailTo,EmailSubject,EmailMessage,EmailHeaders,SendDate) VALUES ('$eid','$email','$subject','$message','$headers',$senddate')");
-    		}
-		}
-		
-		//# urgency == 0 means the user wanted to send the emails immediately
-		if ($urgency == 0) {
-		    mail($email,$subject,$message,$headers);
-		}
+	else {
+	    document.getElementById('ifrbase').src = <?php $_SERVER["DOCUMENT_ROOT"]?>/files/ + m;
+	    document.getElementById('ifrbase').style.visibility = "visible";
+	    document.getElementById('hiddenfile').value = m;
 	}
-?>
+}
+
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
+
+function getTextArea(target) {
+    var i = document.getElementById("txtarea").value;
+    document.getElementById(target).value = i;
+}
+
+function sendNow() {
+    var i = document.getElementById("FileSel").value;
+    document.getElementById("fileselcopy").value = i;
+}
+
+function setMailDetails(target) {
+    var a = document.getElementById("subject").value;
+    var b = document.getElementById("replyto").value;
+    var c = document.getElementById("cc").value;
+    var atarg = "emailsubj" + target;
+    var btarg = "emailrep" + target;
+    var ctarg = "emailcc" + target;
+    document.getElementById(atarg).value = a;
+    document.getElementById(btarg).value = b;
+    document.getElementById(ctarg).value = c;
+}
+</script>
+</head>
+<body>
+<?php
+if (isset($_POST['submit'])) {
+    $emaillist = array();
+    $emaillist = $_POST['list'];
+    $list;
+    $ct = count($emaillist);
+    ?>
+	<div id="container">
+	<div id="left">
+		Email addresses: <textarea id="txtarea" rows="4" cols="55"><?php 
+		for($i=0;$i<$ct;$i++){
+	    echo $emaillist[$i];
+	    $list .= $emaillist[$i];
+	    if($i<$ct-1){
+	        echo ",";
+	        $list .= ",";
+	    }
+	} ?></textarea>
+	<br />
+	File selection: <select id="FileSel" onchange="filedisplay()">
+	<option value="default000"><center> - Select File - </option>
+	<?php
+	$dir = dir($_SERVER['DOCUMENT_ROOT']."/files");
+		while (($file = $dir->read()) !== false) {
+		   if ((substr($file, -4)=="html")) { ?>
+			<option value="<?php echo trim($file) ?>"><?php echo $file ?>
+		   <?php }
+		}
+	$dir->close();
+	?>
+	</select><br />
+	<div>
+	    <fieldset>
+	    <br />
+	        Subject: <br />
+	        <div class="column">
+	        <textarea id="subject" rows="2" cols="45">Message from International Programs and Services</textarea>
+	        </div><br />
+	    <div class="row">
+	        <div class="column2">Reply To: </div>
+	        <div class="column"><input type="text" id="replyto" value="stdyabrd@georgiasouthern.edu"></div><br /><br />
+	    </div>
+	    <div class="row">
+	        <div class="column2">CC: </div>
+	        <div class="column"><input type="text" id="cc" value="stdyabrd@georgiasouthern.edu"></div>
+	    </div>
+	    <br />
+	   </fieldset>
+	</div><br />
+	<p id="test"></p>
+	<form class="fclass" action="emailsys3.php"  target="ifremail" method="post"><br />
+	    <div class="centersubmit">Date to Send (12:00AM): <input type="date" id="start" name="senddate" required
+				   min="<?php echo date("Y-m-d") ?>" max="2100-12-31" /><br />
+	    <input type="hidden" id="hiddenlist" name="emaillist" value="">
+	    <input type="hidden" id="hiddenfile" name="FileSel" value="">
+	    <input type="hidden" id="emailsubj1" name="emailsubj1" value="">
+	    <input type="hidden" id="emailrep1" name="emailrep1" value="">
+	    <input type="hidden" id="emailcc1" name="emailcc1" value=""><br />
+	    <input type="submit" name="preview" value="Preview Email" onclick="getTextArea('hiddenlist'); openTab(event,  'Email');"><br /><br />
+	    <input type="submit" name="schedule" id="schedulesubmit" value="Schedule Emails" onclick="setMailDetails('1')"></div>
+    	<br />
+	</form>
+	    <br />
+    <form class="fclass" action="emailsys3.php"  target="ifremail" method="post">
+    	<br />
+	    <input type="hidden" id="listcopy" name="emaillistcopy" value="">
+	    <input type="hidden" id="fileselcopy" name="fileselcopy" value="">
+	    <input type="hidden" id="emailsubj2" name="emailsubj2" value="">
+	    <input type="hidden" id="emailrep2" name="emailrep2" value="">
+	    <input type="hidden" id="emailcc2" name="emailcc2" value="">
+	    <div class="centersubmit"><input type="submit" class="centersubmit" name="sendnow" value="Send Emails Now" onclick="getTextArea('listcopy'); setMailDetails('2'); openTab(event, 'Email'); sendNow();">
+	    </div>
+	    <br />
+	</form>
+	</div>
+		<div id="middle"></div>
+		<div id="right">
+		<br />
+		<div class="tab">
+			<button class="tablinks" onclick="openTab(event, 'Base')" id="Open">Base Document</button>
+			<button class="tablinks" onclick="openTab(event, 'Email')">Email Preview</button>
+		</div>
+		<div id="Base" class="tabcontent">
+			<p>Base</p>
+			<iframe style="visibility:hidden" width="95%" id="ifrbase" name="ifrbase" src="" frameborder="2" scrolling="no" onload="resizeIframe(this)">
+			</iframe>
+		</div>
+		<div id="Email" class="tabcontent">
+		<p>Email</p>
+			<iframe width="95%" id="ifremail" name="ifremail" src="" frameborder="2" scrolling="no" onload="resizeIframe(this)"></iframe>
+		</div>
+	</div>
+<?php } ?>
+<p id="test" name="testarea"></p>
+</body>
+<script>
+document.getElementById("Open").click();
+</script>
 </html>
